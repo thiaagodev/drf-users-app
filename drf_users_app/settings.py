34d1b8218b266a9ee9776d79649859d8de579f6c
Regis Santos <regis42santos@gmail.com>
2021-07-29 23:48:50 -0300

    Adiciona valores default em settings.

--- drf_users_app/settings.py
+++ drf_users_app/settings.py
@@ -186,9 +186,9 @@ CELERY_BROKER_URL = 'amqp://localhost'
 
 # Email settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = os.getenv('EMAIL_HOST')
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_HOST = os.getenv('EMAIL_HOST', 'localhost')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
+EMAIL_PORT = os.getenv('EMAIL_PORT', 1025)
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
